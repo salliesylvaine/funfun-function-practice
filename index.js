23bf01de97c8bab63f6194f4a8b57ec12020b9c8
Sallie McKee <salliemckee7@gmail.com>
2022-08-29 15:08:18 -0500

    learning how iterators work

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const dragons = ["cool dragon", "angry dragon", "greedy dragon"];
 
 //for...of loops don't know anything about arrays.
 //the array provides an iterator that tells the for...of loop how to iterate it
+const iterator = dragons[Symbol.iterator]()
+iterator {[Iterator]}
 for (const dragon of dragons) {
   dragon;
 }
+
