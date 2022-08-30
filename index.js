d9c3af3521791d57def1008a96858ff23a36df03
Sallie McKee <salliemckee7@gmail.com>
2022-08-30 09:51:27 -0500

    iterators

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const dragons = ["cool dragon", "angry dragon", "greedy dragon"];
 
 //for...of loops don't know anything about arrays.
 //the array provides an iterator that tells the for...of loop how to iterate it
-const iterator = dragons[Symbol.iterator]()
-iterator {[Iterator]}
+
+//symbols are like unique keys, we use them where we normally use a weird unique string to avoid conflict
+Symbol.iterator; //? Symbol {}
+
+const iterator = dragons[Symbol.iterator]();
+iterator.next(); //? {value: 'cool dragon', done: false}
+iterator.next(); //? {value: 'angry dragon', done: false}
+iterator.next(); //? {value: 'greedy dragon', done: false}
+iterator.next(); //? {value: undefined, done: false}
+//this is what the iterator interface looks like. an object that the array spits out
+//that tells things how to iterate over it
+
+for (const char of dragons[0]) {
+  char; //? c, o, o, l, , d, r, a, g, o, n, ....etc.
+}
+
 for (const dragon of dragons) {
   dragon;
 }
-
