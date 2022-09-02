62c5b66a59683962a8c1f44e15177f0dfce63e79
Sallie McKee <salliemckee7@gmail.com>
2022-09-02 12:29:08 -0500

    working on example problems

--- algosudemy.js
+++ algosudemy.js
@@ -321,3 +321,48 @@ let values = [true, {}, [], 2, "awesome"];
 //maybe name function "add", then num1 and num2, and answer is sum
 
 /// EXPLORING EXAMPLES ///
+
+// * coming up with concrete examples can help you understand the problem better
+// * examples also provide sanity checks that your eventual solution works how it should
+//user stories!   unit tests!
+
+// * start with simple examples
+// * progress to more complex examples
+// * explore examples with empty inputs
+// * explore examples with invalid inputs
+
+///// EXAMPLE /////
+//Write a function which takes in a string and returns counts of each character in the string.
+
+// charCount("aaaa"): {a:4}
+// charCount("hello"): {h:1, e:1, l:2, o:1} this raises the question of  "does it need to return all letters regardless or just the letters in the string?"
+// "my phone number is 1284437" should spaces be counted? what about numbers?
+// "Hello hi" uppercase vs lowercase? do they count as the same letter or different?
+// charCount("") what do we want to return if someone inputs an empty string?
+// what happens if someone passes in something that isn't a string?
+
+/// BREAK IT DOWN ///
+
+// * explicitly write out the steps you need to take.
+// this forces you to think about the code you'll write before you write it, and helps you catch
+// any lingering conceptual issues or misunderstandings before you dive in and have to worry about
+// details (e.g. language syntax) as well. this also shows the interviewer how you think, how you
+// go about problem solving, etc. instead of you standing there silently.
+
+///// EXAMPLE /////
+//Write a function which takes in a string and returns counts of each character in the string.
+
+function charCount(str) {
+  //do something
+  //return an object with keys that are lowercase alphanumeric characters in the string;
+  //values should be the counts for those characters
+}
+
+function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character...
+  //if the char is a number/letter AND is a key in object, add one to count
+  //if the char is a number/letter AND not in object, add it and set value to 1
+  //if character is something else (space, period, etc.) don't do anything
+  // return object at end
+}
