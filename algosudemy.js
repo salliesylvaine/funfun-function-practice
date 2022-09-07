df71f5cfbed684826b39577dc73f6a54adfb153f
Sallie McKee <salliemckee7@gmail.com>
2022-09-07 09:50:36 -0500

    simplifying and solving problems

--- algosudemy.js
+++ algosudemy.js
@@ -366,3 +366,62 @@ function charCount(str) {
   //if character is something else (space, period, etc.) don't do anything
   // return object at end
 }
+
+/// SOLVE THE PROBLEM ///
+// or solve a simpler problem //
+
+/// SIMPLIFY ///
+// * find the core difficulty in what you're trying to do
+// * temporarily ignore that difficulty
+// * write a simplified solution
+// * then incorporate that difficulty
+
+///// EXAMPLE /////
+//Write a function which takes in a string and returns counts of each character in the string.
+
+function charCount(str) {
+  // make object to return at end
+  var result = {};
+  // loop over string, for each character...
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    //if the char is a number/letter AND is a key in object, add one to count
+    if (result[char] > 0) {
+      result[char]++;
+    }
+    //if the char is a number/letter AND not in object, add it and set value to 1
+    else {
+      result[char] = 1;
+    }
+  }
+
+  //if character is something else (space, period, etc.) don't do anything
+  // return object at end
+  return result;
+}
+
+// output:
+// charCount('hello')
+//{h: 1, e: 1, l: 2, o: 1}
+
+//charCount('Hi there!')
+// {H: 1, i: 1, " ": 1, t: 1, h: 1, …}
+// " ": 1
+// !: 1
+// H: 1
+// e: 2
+// h: 1
+// i: 1
+// r: 1
+// t: 1
+
+// adding .toLowerCase()
+// charCount("Hi there!")
+// {h: 2, i: 1, " ": 1, t: 1, e: 2, …}
+// " ": 1
+// !: 1
+// e: 2
+// h: 2
+// i: 1
+// r: 1
+// t: 1
