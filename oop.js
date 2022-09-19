11b307f610bf7f9d8e5dfdfd37c8c24e8cbc7eff
Sallie McKee <salliemckee7@gmail.com>
2022-09-19 14:57:35 -0500

    learning about objects

--- oop.js
+++ oop.js
@@ -2,3 +2,13 @@
 
 // Objects in JS are quite like objects in real life
 // They have properties and things they can do (methods)
+
+var names = ['ryu', 'crystal', 'mario']
+
+//technically, names will come back as an array that is an object
+['ryu', 'crystal', 'mario']
+0: 'ryu'
+1: 'crystal'
+2: 'mario'
+length: 3
+//if you called names.length, you would get 3
