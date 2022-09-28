6e73a72f532c97ef4c4bcc41490f160dfa71207f
Sallie McKee <salliemckee7@gmail.com>
2022-09-28 12:16:31 -0500

    pure recursion

--- algosudemy.js
+++ algosudemy.js
@@ -975,3 +975,44 @@ function collectOddValues(arr) {
 
   return result;
 }
+
+/// PURE RECURSION ///
+function collectOddValues(arr) {
+  let newArr = [];
+
+  if (arr.length == 0) {
+    //base case
+    return newArr;
+  }
+
+  if (arr[0] % 2 !== 0) {
+    //check if first element is odd
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+
+  return newArr;
+}
+
+//collectOddValues([1,2,3,4,5]); this would make it:
+// [1].concat(collectOddValues([2,3,4,5]))
+///////////////// [].concat(collectOddValues([3,4,5]))
+/////////////////////////////// [3].concat(collectOddValues([4,5]))
+////////////////////////////////////////////// [].concat(collectOddValues([5]))
+/////////////////////////////////////////////////////////// [5].concat(collectOddValues([]))
+/////////////////////////////////////////////////////////////////////////// returns empty []
+
+// BUT! concat, working backwards, returns[1,3,5]
+// [5].concat([]) = [5]
+// [].concat([5]) =  [5]
+// [3].concat([5]) = [3,5]
+// [].concat([3,5]) = [3,5]
+// [1].concat([3,5]) = [1,3,5]
+
+/// PURE RECURSION TIPS ///
+// * For arrays, use methods like slice, the spread operator, and concat
+// that make copies of arrays so you do not mutate them
+// * Remember that strings are immutable so you will need to use methods
+// like slice, substr, or substring to make copies of strings
+// * To make copies of objects use Object.assign, or the spread operator
