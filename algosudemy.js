c718b1d8d029343a4bb7e433311d524c0826341a
Sallie McKee <salliemckee7@gmail.com>
2022-10-13 14:51:12 -0500

    linear search practice

--- algosudemy.js
+++ algosudemy.js
@@ -1136,3 +1136,18 @@ function reverse(str) {
 
 //But how do these functions work?
 // they're doing linear search
+
+//Linear Search Pseudocode
+// * this function accepts an array and a value
+// * loop through the array and check if the current array element is equal to the value
+// * if it is, return the index at which the element is found
+// * if the value is never found, return -1
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return -1;
+}
+//Big O - O(n)
