ebda1e5b2f6bef5b76b59d979af080cd6953a4d1
Sallie McKee <salliemckee7@gmail.com>
2022-10-31 15:30:25 -0400

    binary search example

--- algosudemy.js
+++ algosudemy.js
@@ -1174,3 +1174,38 @@ function linearSearch(arr, val) {
 [1, 3, 4, 6, 8, 9, 11, 12, 15, 16, 17, 18, 19];
 //            left (12)  middle (15) right (16)
 //this only took 3 guesses
+
+/// Binary Search Pseudocode
+// * this function accepts a sorted array and a value
+// * create a left pointer at the start of the array, and a right pointer
+// at the end of the array
+// * while the left pointer comes before the right pointer:
+//    * create a pointer in the middle
+//    * if you find the value you want, return the index
+//    * if the value is too small, move the left pointer up
+//    * if the value us too large, move the right pointer down
+// * if you never find the value, return -1
+
+function binarySearch(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  if (arr[middle] === elem) {
+    return middle;
+  }
+  return -1;
+
+  //refactor:
+  //return arr[middle] === elem ? middle : -1;
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15);
