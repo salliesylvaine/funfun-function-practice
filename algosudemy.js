30b416040717cfecad51436ce40ea61984b0288c
Sallie McKee <salliemckee7@gmail.com>
2022-11-01 14:42:15 -0400

    string search implementation

--- algosudemy.js
+++ algosudemy.js
@@ -1228,3 +1228,20 @@ binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15);
 // * if you complete the inner loop and find a match, increment
 // the count of matches
 // * return the count
+
+function naiveSearch(long, short) {
+  let count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) {
+        break;
+      }
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+naiveSearch("lorie loled", "lol");
