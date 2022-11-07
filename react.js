10f44c02f8fcadd619982c999f419b487642f259
Sallie McKee <salliemckee7@gmail.com>
2022-11-07 09:28:13 -0600

    components

--- react.js
+++ react.js
@@ -3,7 +3,9 @@
 // 1. Don't touch the DOM, I'll do it.
 //     -Document Object Model, what the browser uses to display a web app.
 //     JS just manipulates the DOM. It's a tree representation of the page.
-
+// 2. Building websites like lego blocks.
+//     -Components!! are Javascript functions created from the state data. It's a function that receives attributes,
+//      called props, and returns html that's inside javascript
 /// Declarative vs Imperative
 
 // In an Imperative Paradigm, you directly change individual parts of your app in response to various user events.
@@ -12,3 +14,4 @@
 // In Declarative paradigm, react gets a blueprint in the form of a javascript object of how the page should look,
 // then react finds the best way to manipulate the DOM. In essence, we declare what the state should be like, and then just make changes
 // based on state.
+// less complexity, better code quality, faster developer times
