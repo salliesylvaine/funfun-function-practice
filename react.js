3c426fc83069419a356a5398f11ac59263848483
Sallie McKee <salliemckee7@gmail.com>
2022-11-07 09:21:29 -0600

    declarative vs imperative

--- react.js
+++ react.js
@@ -3,3 +3,12 @@
 // 1. Don't touch the DOM, I'll do it.
 //     -Document Object Model, what the browser uses to display a web app.
 //     JS just manipulates the DOM. It's a tree representation of the page.
+
+/// Declarative vs Imperative
+
+// In an Imperative Paradigm, you directly change individual parts of your app in response to various user events.
+// hard to debug
+
+// In Declarative paradigm, react gets a blueprint in the form of a javascript object of how the page should look,
+// then react finds the best way to manipulate the DOM. In essence, we declare what the state should be like, and then just make changes
+// based on state.
