5baa14167026c9b613822d7c874521e375659830
Sallie McKee <salliemckee7@gmail.com>
2022-11-07 09:09:00 -0600

    starting react

---
+++ react.js
@@ -0,0 +1,5 @@
+/////  REACT CONCEPTS /////
+
+// 1. Don't touch the DOM, I'll do it.
+//     -Document Object Model, what the browser uses to display a web app.
+//     JS just manipulates the DOM. It's a tree representation of the page.
