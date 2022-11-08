95cf2ced47a899fd936b63b29268729f6b361d91
Sallie McKee <salliemckee7@gmail.com>
2022-11-08 08:54:45 -0600

    starting node

---
+++ node.js
@@ -0,0 +1,7 @@
+/// What is Node.js? ///
+
+// allows developers to use JavaScript on the server side (instead of being forced to run it on the client)
+
+// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. The point of the engine is to take JS code
+// and compile it down to machine code that your machine can execute.
+// The runtime is something that provides custom functionality (tools and libraries)
