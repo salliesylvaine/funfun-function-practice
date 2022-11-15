ba5ec7d78e85b49437851e2370f21113ea615ada
Sallie McKee <salliemckee7@gmail.com>
2022-11-15 09:34:40 -0600

    bubblesort optimization

--- algosudemy.js
+++ algosudemy.js
@@ -1377,3 +1377,22 @@ function bubbleSort(arr) {
   }
   return arr;
 }
+
+/// BUBBLE SORT OPTIMIZATION ///
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i < 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // SWAP!
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break; // this stops the code from running after everything is sorted
+  }
+  return arr;
+}
